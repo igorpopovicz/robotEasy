03f381bd50cfdc41ec0aeababd4c3e923ba41050
guirgo <igorpopo2002@gmail.com>
2024-08-21 20:15:00 -0300

    add api

---
+++ robot_easy_api/src/main/java/com/guirgo/robot_easy_api/RobotEasyApiApplication.java
@@ -0,0 +1,13 @@
+package com.guirgo.robot_easy_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RobotEasyApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RobotEasyApiApplication.class, args);
+	}
+
+}
---
+++ robot_easy_api/src/test/java/com/guirgo/robot_easy_api/RobotEasyApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.guirgo.robot_easy_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RobotEasyApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
