080d263ec34ed4b72ae38e006bbf91fdafe686d7
guirgo <igorpopo2002@gmail.com>
2024-08-21 22:03:50 -0300

    change get cep to receive cep from request body

--- robot_easy_api/src/main/java/com/guirgo/robot_easy_api/CepController.java
+++ robot_easy_api/src/main/java/com/guirgo/robot_easy_api/CepController.java
@@ -4,8 +4,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.guirgo.robot_easy_api.CepRequest;
 
 @RestController
 @RequestMapping("/api/cep")
@@ -14,8 +17,9 @@ public class CepController {
     @Autowired
     private CepService cepService;
 
-    @GetMapping("/{cep}")
-    public ResponseEntity<CepModel> getCep(@PathVariable String cep) {
-        return ResponseEntity.ok(cepService.getCep(cep));
+    @GetMapping("/get")
+    public ResponseEntity<CepModel> getCep(@RequestBody CepRequest cepRequest) {
+
+        return ResponseEntity.ok(cepService.getCep(cepRequest.getCep()));
     }
 }
---
+++ robot_easy_api/src/main/java/com/guirgo/robot_easy_api/CepRequest.java
@@ -0,0 +1,13 @@
+package com.guirgo.robot_easy_api;
+
+public class CepRequest {
+    private String cep;
+
+    public String getCep() {
+        return this.cep;
+    }
+
+    public void setCep(String cep) {
+        this.cep = cep;
+    }
+}
